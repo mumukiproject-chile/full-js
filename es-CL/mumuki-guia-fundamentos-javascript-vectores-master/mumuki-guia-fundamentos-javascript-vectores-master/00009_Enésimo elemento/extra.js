e03f04964f0d8a1225e8a325b6756f1a010c4971
Agustin Pina <aguspina87@gmail.com>
2020-06-24 14:12:22 -0300

    New translations extra.js (Spanish, Chile)

---
+++ es-CL/mumuki-guia-fundamentos-javascript-vectores-master/mumuki-guia-fundamentos-javascript-vectores-master/00009_Enésimo elemento/extra.js
@@ -0,0 +1,14 @@
+let mesesDelAnio = [
+    'enero',
+    'febrero',
+    'marzo',
+    'abril',
+    'mayo',
+    'junio',
+    'julio',
+    'agosto',
+    'septiembre',
+    'octubre',
+    'noviembre',
+    'diciembre'
+];
