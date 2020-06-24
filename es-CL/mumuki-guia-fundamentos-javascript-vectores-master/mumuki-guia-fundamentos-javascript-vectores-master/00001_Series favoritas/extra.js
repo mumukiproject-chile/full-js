d2d87476eda8bb842b01ed77f585f65ffab7938e
Agustin Pina <aguspina87@gmail.com>
2020-06-24 14:12:28 -0300

    New translations extra.js (Spanish, Chile)

---
+++ es-CL/mumuki-guia-fundamentos-javascript-vectores-master/mumuki-guia-fundamentos-javascript-vectores-master/00001_Series favoritas/extra.js
@@ -0,0 +1,9 @@
+var seriesFavoritasDeAna = [
+    'Game of Thrones',
+    'Breaking Bad',
+    'House of Cards'
+];
+var seriesFavoritasDeHector = [
+    'En Terapia',
+    'Recordando el Show de Alejandro Molina'
+];
