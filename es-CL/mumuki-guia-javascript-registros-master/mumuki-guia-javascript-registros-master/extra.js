eac6fdb35e5ea63b29b72c4f08bbe039d6e9563f
Agustin Pina <aguspina87@gmail.com>
2020-06-24 14:07:22 -0300

    New translations extra.js (Spanish, Chile)

---
+++ es-CL/mumuki-guia-javascript-registros-master/mumuki-guia-javascript-registros-master/extra.js
@@ -0,0 +1,12 @@
+function longitud(lista) {
+    return lista.length;
+}
+function agregar(lista, elemento) {
+    return lista.push(elemento);
+}
+function remover(lista) {
+    return lista.pop();
+}
+function posicion(lista, elemento) {
+    return lista.indexOf(elemento);
+}
